9f4f873f1195c72de4d43de261abab30c8c20333
Franco Ortega <francopdx@gmail.com>
2021-02-16 13:50:42 -0800

    add test for color change

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, screen, fireEvent, cleanup } from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
   afterEach(() => cleanup());
-  it('renders App', () => {
-    const { asFragment } = render(<App />);
-    expect(asFragment()).toMatchSnapshot();
+  it('changes the color', async() => {
+    render(<App />);
+
+    const current = screen.getByTestId('current');
+    
+    fireEvent.change(current, {
+      target: {
+        value: '#FF0000'
+      }
+    });
+
+    const display = await screen.findByTestId('display');
+        
+    expect(display).toHaveStyle({
+      backgroundColor: '#FF0000',
+    });
   });
 });
+
+
+// const before = screen.getByTestId('before');
+// const after = screen.getByTestId('after');
+
+// fireEvent.change(before, {
+//   target: {
+//     value: '#00FF00'
+//   }
+// });
+
+
+// fireEvent.change(after, {
+//   target: {
+//     value: '#0000FF'
+//   }
+// });
+
+// before: '#00FF00',
+// after: '#FF0000'
