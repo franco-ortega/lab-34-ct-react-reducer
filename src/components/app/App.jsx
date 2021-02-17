5bdc5019ba243da7f735f77b3dc8fa8f59a0c8bc
Franco Ortega <francopdx@gmail.com>
2021-02-16 17:50:08 -0800

    refactor App to have it be more similar to how it was at the beginning

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -4,26 +4,33 @@ import reducer, { initialState } from '../../hooks/useReducer';
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const undo = () => {
+    dispatch({
+      type: 'UNDO'
+    });
+  };
+
+  const redo = () => {
+    dispatch({
+      type: 'REDO'
+    });
+  };
+
+  const current = ({ target }) => {
+    dispatch({
+      type: 'CURRENT_CHANGE', 
+      payload: target.value });
+  };
+
   return (
     <>
-      <button data-testid="before" onClick={() => dispatch({
-        type: 'UNDO'
-      })}>undo</button>
-      <button data-testid="after" onClick={() => dispatch({
-        type: 'REDO'
-      })}>redo</button>
-      <input
-        data-testid="current"
-        type="color"
-        value={state.current}
-        onChange={({ target }) => dispatch({
-          type: 'CURRENT_CHANGE', 
-          payload: target.value })
-        }
+      <button onClick={undo}>undo</button>
+      <button onClick={redo}>redo</button>
+      <input data-testid="current"
+        type="color" value={state.current} onChange={current}
       />
       <div data-testid="display"
-        style={{ 
-          backgroundColor: state.current, 
+        style={{  backgroundColor: state.current, 
           width: '10rem', 
           height: '10rem' }}
       ></div>
